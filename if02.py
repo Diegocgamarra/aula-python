3eb6d7fb3292a86c2b5d6ee92153b7ca47d3b509
diegogamarra <diegogamarra@msn.com>
2025-03-31 13:44:29 -0400

    Primeiro commit

---
+++ if02.py
@@ -0,0 +1,9 @@
+num1 = int(input("Digite um número"))
+num2 = int(input("Digite o segundo número"))
+if num1 == num2:
+    print("Os números são iguais")
+if num1 > num2:
+    print(f"{num1} é maior que {num2}")
+if num1 < num2:
+    print(f"{num2} é maior que {num1}")
+print("Fim do Programa")
